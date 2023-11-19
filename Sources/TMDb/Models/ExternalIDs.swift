2ab635a0701734ad1eda9ea28f3d0dba385e0302
theonlymo <145556517+theonlymo@users.noreply.github.com>
2023-11-19 21:16:26 +0100

    add codable

--- Sources/TMDb/Models/ExternalIDs.swift
+++ Sources/TMDb/Models/ExternalIDs.swift
@@ -29,6 +29,22 @@ public struct ExternalIDs: Identifiable, Codable, Equatable, Hashable {
         case imdbId = "imdb_id"
     }
     
+    
+    public init(from decoder: Decoder) throws {
+        let container: KeyedDecodingContainer<ExternalIDs.CodingKeys> = try decoder.container(keyedBy: ExternalIDs.CodingKeys.self)
+        
+        self.id = try container.decode(Int.self, forKey: ExternalIDs.CodingKeys.id)
+        self.imdbId = try container.decode(String.self, forKey: ExternalIDs.CodingKeys.imdbId)
+        
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<ExternalIDs.CodingKeys> = encoder.container(keyedBy: ExternalIDs.CodingKeys.self)
+        
+        try container.encode(self.id, forKey: ExternalIDs.CodingKeys.id)
+        try container.encode(self.imdbId, forKey: ExternalIDs.CodingKeys.imdbId)
+    }
+    
 
 }
 
