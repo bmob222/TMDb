31c0013eb157ca10e78527cf634e4c7ee42c0448
theonlymo <145556517+theonlymo@users.noreply.github.com>
2023-11-27 22:09:26 +0100

    Revert "add tvdbId"
    This reverts commit 78f92e52f7b23e9080499f55a16ffe2ae842739d.

--- Sources/TMDb/Models/ExternalIDs.swift
+++ Sources/TMDb/Models/ExternalIDs.swift
@@ -2,37 +2,49 @@ import Foundation
 
 public struct ExternalIDs: Identifiable, Codable, Equatable, Hashable {
 
+    ///
+    /// Genre Identifier.
+    ///
     public let id: Int
+
+    ///
+    /// Genre name.
+    ///
     public let imdbId: String
-    public let tvdbId: Int?
-    
-    public init(id: Int, imdbId: String, tvdbId: Int? = nil) {
+
+    ///
+    /// Creates a genre object.
+    ///
+    /// - Parameters:
+    ///    - id: Genre Identifier.
+    ///    - name: Genre name.
+    ///
+    public init(id: Int, imdbId: String) {
         self.id = id
         self.imdbId = imdbId
-        self.tvdbId = tvdbId
     }
     
     enum CodingKeys: String, CodingKey {
         case id
         case imdbId = "imdbId"
-        case tvdbId = "tvdbId"
     }
     
     
     public init(from decoder: Decoder) throws {
         let container: KeyedDecodingContainer<ExternalIDs.CodingKeys> = try decoder.container(keyedBy: ExternalIDs.CodingKeys.self)
+        
         self.id = try container.decode(Int.self, forKey: ExternalIDs.CodingKeys.id)
         self.imdbId = try container.decode(String.self, forKey: ExternalIDs.CodingKeys.imdbId)
-        self.tvdbId = try container.decode(Int.self, forKey: ExternalIDs.CodingKeys.tvdbId)
+        
     }
     
     public func encode(to encoder: Encoder) throws {
         var container: KeyedEncodingContainer<ExternalIDs.CodingKeys> = encoder.container(keyedBy: ExternalIDs.CodingKeys.self)
+        
         try container.encode(self.id, forKey: ExternalIDs.CodingKeys.id)
         try container.encode(self.imdbId, forKey: ExternalIDs.CodingKeys.imdbId)
-        try container.encode(self.tvdbId, forKey: ExternalIDs.CodingKeys.tvdbId)
     }
     
-    
+
 }
 
